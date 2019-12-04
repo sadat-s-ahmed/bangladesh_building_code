52d9047b1ba78eb783e096edd762b90f687f3979
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-12-04 11:20:08 +0600

    ABOUT PAGE DONE

--- bd_building_code/lib/pages/about/about_page.dart
+++ bd_building_code/lib/pages/about/about_page.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:pdf_render/pdf_render.dart';
+import 'package:pdf_render/pdf_render_widgets.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
+import 'package:device_info/device_info.dart';
 
-Color backgroundColor = Colors.grey.shade200;
+
 Color appbarColor = Colors.black;
-Color bg_grad = Color.fromRGBO(58, 58,58,1);
-Color bg_grad2 = Color.fromRGBO(58, 58, 58,.7);
-Color grads = Color.fromRGBO(189 , 195 , 199, 1);
-Color grads2 = Color.fromRGBO(44  , 62  , 80, 1);
-Color gradinner1 = Color.fromRGBO(142 , 158 , 171, 1);
-Color gradinner2 = Color.fromRGBO(238, 242 , 243, 1);
-
-
 
 class AboutPage extends StatefulWidget {
   AboutPage({Key key}) : super(key: key);
@@ -21,165 +18,76 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
-List list   ;
-String text ; 
-@override
-void initState() { 
-  super.initState();
-  getBearings();
-  
-}
-getBearings() async{
-  loadAsset(context).then((s){
-    print(s.split("CHAPTER").length);
 
-    list = s.split('###SECTION');
-    text = s.split('###SECTION')[0] ; 
-  });
-}
-
-Future<String> loadAsset(BuildContext context) async {
-  return await DefaultAssetBundle.of(context).loadString('assets/BNBC.txt');
-}
-
-TextStyle posRes = TextStyle(color: Colors.white, backgroundColor: Colors.red),
-    negRes = TextStyle(color: Colors.black, backgroundColor: Colors.white);
-
-String search;
-
-
-// final text = '''
-// Call me Ishmael. Some years ago—never mind how long precisely—having
-// little or no money in my purse, and nothing particular to interest me
-// on shore, I thought I would sail about a little and see the watery part
-// of the world. It is a way I have of driving off the spleen and
-// regulating the circulation. Whenever I find myself growing grim about
-// the mouth; whenever it is a damp, drizzly November in my soul; whenever
-// I find myself involuntarily pausing before coffin warehouses, and
-// bringing up the rear of every funeral I meet; and especially whenever
-// my hypos get such an upper hand of me, that it requires a strong moral
-// principle to prevent me from deliberately stepping into the street, and
-// methodically knocking people’s hats off—then, I account it high time to
-// get to sea as soon as I can. This is my substitute for pistol and ball.
-// With a philosophical flourish Cato throws himself upon his sword; I
-// quietly take to the ship. There is nothing surprising in this. If they
-// but knew it, almost all men in their degree, some time or other,
-// cherish very nearly the same feelings towards the ocean with me.
-// '''.replaceAll("\n", " ").replaceAll("  ", "");
-
-TextSpan searchMatch(String match) {
-  if (search == null || search == "")
-    return TextSpan(text: match, style: negRes);
-  var refinedMatch = match.toLowerCase();
-  var refinedSearch = search.toLowerCase();
-  if (refinedMatch.contains(refinedSearch)) {
-    if (refinedMatch.substring(0, refinedSearch.length) == refinedSearch) {
-      return TextSpan(
-        style: posRes,
-        text: match.substring(0, refinedSearch.length),
-        children: [
-          searchMatch(
-            match.substring(
-              refinedSearch.length,
-            ),
-          ),
-        ],
-      );
-    } else if (refinedMatch.length == refinedSearch.length) {
-      return TextSpan(text: match, style: posRes);
-    } else {
-      return TextSpan(
-        style: negRes,
-        text: match.substring(
-          0,
-          refinedMatch.indexOf(refinedSearch),
-        ),
-        children: [
-          searchMatch(
-            match.substring(
-              refinedMatch.indexOf(refinedSearch),
-            ),
-          ),
-        ],
-      );
-    }
-  } else if (!refinedMatch.contains(refinedSearch)) {
-    return TextSpan(text: match, style: negRes);
-  }
-  return TextSpan(
-    text: match.substring(0, refinedMatch.indexOf(refinedSearch)),
-    style: negRes,
-    children: [
-      searchMatch(match.substring(refinedMatch.indexOf(refinedSearch)))
-    ],
-  );
-}
-
+final pageController = PageController(
+  initialPage: 0 ,
+);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
             appBar:PreferredSize(
-                preferredSize: Size.fromHeight(60.0),
-                  child: AppBar(
-                  backgroundColor:appbarColor ,
-                  elevation: 0.0,
-                  centerTitle: true,
-                  title: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    color: Colors.white,
-                    child: TextField(
-                      showCursor: true,
-                      style: TextStyle(fontSize: 22 ,color:  Colors.black ),
-                      decoration: InputDecoration(hintText: "Search"),
-                      onChanged: (t) {
-                        setState(() => search = t);
-                      },
-                    ),
-                  ),
-                ),
-            ), 
-            drawer: new Drawer(
-              child: FutureBuilder(
-                future: loadAsset(context),
-                builder: (context , snapshot){
-                  if(snapshot.hasData){
-                    
-                    return new ListView.builder(
-                      itemCount: snapshot.data.split('###SECTION').length,
-                      itemBuilder: (context , index ){
-                        return ListTile(
-                            title: new Text("Chapter $index"),
-                            onTap: () {
-                              
-                              setState(() {
-                                 text= list[index];
-                              });
-                            });
-                      },
-                    );
-                  }else{
-                    return Center(child: CircularProgressIndicator(),);
-                  }
-                },
+          preferredSize: Size.fromHeight(60.0),
+            child: AppBar(
+            backgroundColor:appbarColor ,
+            elevation: 0.0,
+            centerTitle: true,
+            title: new Text(
+                'About Us',
+                style: TextStyle(
+                     fontWeight: FontWeight.bold,
+                     color: Colors.white
+                     ),
               ),
+          ),
       ),
-            body: Scrollbar(
-              child: SingleChildScrollView(
-                child: RichText(
-                      textScaleFactor: 1,
-                      text: searchMatch(
-                       text,
-                      ),
-                    )
-                )
-                
-                
-                ,
-              ),
-          );
-  }
+            body: FutureBuilder<PDFDocument>(
+            future: _getDocument(),
+            builder: (_, snapshot) {
+              if (snapshot.hasData) {
+              
+                return PDFView(
+                  controller: pageController,
+                  document: snapshot.data,
+                );
+              }
 
-  
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    'chole nah/',
+                  ),
+                );
+              }
+
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+    );
+  }
+    Future<bool> _hasSupportPdfRendering() async {
+      final deviceInfo = DeviceInfoPlugin();
+      bool hasSupport = false;
+      if (Platform.isIOS) {
+        final iosInfo = await deviceInfo.iosInfo;
+        hasSupport = int.parse(iosInfo.systemVersion.split('.').first) >= 11;
+      } else if (Platform.isAndroid) {
+        final androidInfo = await deviceInfo.androidInfo;
+        hasSupport = androidInfo.version.sdkInt >= 21;
+      }
+      return hasSupport;
+    }
+
+  Future<PDFDocument> _getDocument() async {
+      if (await _hasSupportPdfRendering()) {
+        return PDFDocument.openAsset('assets/ABOUT.pdf');
+      } else {
+        throw Exception(
+          'PDF Rendering does not '
+          'support on the system of this version',
+        );
+      }
+    }
 }
+
